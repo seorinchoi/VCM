3cf02c0bc577dccdbaa6eede6b2ca67e9f0b00e9
seorinchoi <89957826+seorinchoi@users.noreply.github.com>
2024-11-08 21:38:00 +0900

    Update segmenter_vit-t_mask_8xb1-160k_ade20k-512x512.py

--- segmenter_vit-t_mask_8xb1-160k_ade20k-512x512.py
+++ segmenter_vit-t_mask_8xb1-160k_ade20k-512x512.py
@@ -9,17 +9,69 @@ data_preprocessor = dict(size=crop_size)
 checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segmenter/vit_tiny_p16_384_20220308-cce8c795.pth'  # noqa
 
 model = dict(
-    data_preprocessor=data_preprocessor,
-    pretrained=checkpoint,
-    backbone=dict(embed_dims=192, num_heads=3),
-    decode_head=dict(
-        type='SegmenterMaskTransformerHead',
-        in_channels=192,
-        channels=192,
+    backbone=dict(
+        attn_drop_rate=0.0,
+        drop_path_rate=0.1,
+        drop_rate=0.0,
+        embed_dims=192,
+        final_norm=True,
+        img_size=(
+            512,
+            512,
+        ),
+        in_channels=3,
+        interpolate_mode='bicubic',
+        norm_cfg=dict(requires_grad=True, type='LN'),
         num_heads=3,
-        embed_dims=192))
+        num_layers=12,
+        patch_size=8, #patch size 8 
+        type='VisionTransformer',
+        with_cls_token=True),
+    data_preprocessor=dict(
+        bgr_to_rgb=True,
+        mean=[
+            127.5,
+            127.5,
+            127.5,
+        ],
+        pad_val=0,
+        seg_pad_val=255,
+        size=(
+            256,
+            256,
+        ),
+        std=[
+            127.5,
+            127.5,
+            127.5,
+        ],
+        type='SegDataPreProcessor'),
+    decode_head=dict(
+        channels=192,
+        dropout_ratio=0.0,
+        embed_dims=192,
+        in_channels=192,
+        loss_decode=dict(
+            loss_weight=1.0, type='CrossEntropyLoss', use_sigmoid=False),
+        norm_cfg=dict(requires_grad=True, type='LN'),
+        num_classes=3,
+        num_heads=3,
+        num_layers=2,
+        out_channels=3,
+        threshold=0.3,
+        type='SegmenterMaskTransformerHead'),
+    pretrained=
+    'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segmenter/vit_tiny_p16_384_20220308-cce8c795.pth',
+    test_cfg=dict(crop_size=(
+        512,
+        512,
+    ), mode='slide', stride=(
+        480,
+        480,
+    )),
+    type='EncoderDecoder')
 
-train_dataloader = dict(batch_size=8) #batch-size
+train_dataloader = dict(batch_size=16) #batch-size
 val_dataloader = dict(batch_size=1)
 
 
