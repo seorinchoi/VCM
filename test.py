0cc243d5e2a17a0ff98626662e3b90e42713274f
seorinchoi <89957826+seorinchoi@users.noreply.github.com>
2024-11-10 19:38:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import os.path as osp
 import mmseg.datasets.KiTS21Datasets
+import mmseg.datasets.SMCDatasets
 from mmengine.config import Config, DictAction
 from mmengine.runner import Runner
 
