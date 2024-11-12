78429666a8f2808934da639b51f4192a3ff0f528
seorinchoi <89957826+seorinchoi@users.noreply.github.com>
2024-11-12 14:19:16 +0900

    Update segmenter_vit-t_mask_8xb1-160k_ade20k-512x512.py
    loss change

--- segmenter_vit-t_mask_8xb1-160k_ade20k-512x512.py
+++ segmenter_vit-t_mask_8xb1-160k_ade20k-512x512.py
@@ -51,8 +51,10 @@ model = dict(
         dropout_ratio=0.0,
         embed_dims=192,
         in_channels=192,
-        loss_decode=dict(
-            loss_weight=1.0, type='DiceLoss', use_sigmoid=False),
+    loss_decode=[
+        dict(type='CrossEntropyLoss', loss_name='loss_ce', use_sigmoid=False, loss_weight=1.0),
+        dict(type='DiceLoss', loss_name='loss_dice', loss_weight=3.0)
+    ],
         norm_cfg=dict(requires_grad=True, type='LN'),
         num_classes=3,
         num_heads=3,
